a36329839a2fe3a3ba34c43887d1352720e44298
nishmistry <mistry.nish@northeastern.edu>
2023-07-17 20:42:20 -0400

    responsive css for about section

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,6 @@ function Home() {
       <h1>Hey, I'm Nish Mistry.</h1>
       <h1 className="highlighted-hero-text">I like solving <span className="highlighted">problems</span>.</h1>
       <a className="about-button" href="#about">get to know me</a>
-
       <div className="circle circle-1"></div>
       <div className="circle circle-2"></div>
       <div className="circle circle-3"></div>
