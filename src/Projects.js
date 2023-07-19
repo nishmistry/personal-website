1b868ac43a0edac27555e7c5392742792713d5b4
nishmistry <mistry.nish@northeastern.edu>
2023-07-19 19:42:15 -0400

    started working on projects section

--- src/Projects.js
+++ src/Projects.js
@@ -6,18 +6,22 @@ function Projects() {
   return (
     <div id="projects">
       <h2 className="projects-header">projects</h2>
-      <div className="project-container">
+      <div className="projects-container">
       {projectsData.map((project) => (
           <div className="project" key={project.id}>
-            <img src={project.imageSrc} alt={project.altText} />
-            <h3>{project.title}</h3>
-            <p>{project.description}</p>
-            <a href={project.liveLink} target="_blank" rel="noopener noreferrer">
-              View Live
-            </a>
-            <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
-              GitHub
-            </a>
+            <div className="project-img">
+              <img src={project.imageSrc} alt={project.altText} />
+            </div>
+            <div className="project-info">
+              <h3>{project.title}</h3>
+              <p>{project.description}</p>
+              <a href={project.liveLink} target="_blank" rel="noopener noreferrer">
+                View Live
+              </a>
+              <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
+                GitHub
+              </a>
+            </div>
           </div>
         ))}
       </div>
