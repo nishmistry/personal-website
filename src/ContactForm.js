594c1ab6a6cbc39f5459c49f76648b6f0ea188c5
nishmistry <mistry.nish@northeastern.edu>
2023-07-22 21:36:45 -0400

    netlify form handling added

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -30,7 +30,7 @@ function ContactForm() {
       <h2 className="contact-header">contact</h2>
       <h2 className="contact-cta">like what you see? let's get in <span className="highlighted">touch</span>.</h2>
       <div id="contact-form-container">
-        <form onSubmit={handleSubmit} name="contact" data-netlify="true">
+        <form onSubmit={handleSubmit} name="contact" netlify>
           <div className="form-field">
             <input
               type="text"
